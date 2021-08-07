8dd1951578f09d1a3eda7259747c9697fe43bceb
mai7855 <sam7855@gmail.com>
2021-08-07 12:59:47 +0800

    Update ServletServer.java
    modify MessageServlet addInitParam.

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World")
+                                    .addInitParam("message", "Hello World change by mai7855 on RedHat OpenShift CodeReady")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
